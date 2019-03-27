61ef8ada3022e2433926ffbbeecffaafe1acb71d
xuruiting1 <1263183452@qq.com>
2019-03-27 17:09:41 +0800

    add v1.0
    Signed-off-by: xuruiting1 <1263183452@qq.com>

--- rectangle.js
+++ rectangle.js
@@ -1,20 +1,51 @@
 
+function valid(data){
+
+}
 $(function() {
     var $width = $('#width'),
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $area = $('#area');
+      $widthValidation=$('#width-validation');
+      $heightValidation=$('#height-validation');
 
+      $area = $('#area');
+      $width.focusout(function(){
+      var w=$width.val();
+
+      if(w===''){
+        $widthValidation.html('宽度不能为空');
+      }else{
+         $widthValidation.html();
+      }
+    });
+    $height.focusout(function(){
+      var h=$height.val();
+
+      if(h===''){
+        $heightValidation.html('高度不能为空');
+        $height.select();
+      }else{
+         $heightValidation.html();
+      }
+
+    });
     $btnCal.click(function(){
       var w = $width.val(),
           h = $height.val();
-     
-      var p = 2 * (w + h),
+       if(w===''){
+        $widthValidation.html('宽度不能为空');
+      }else if(h===''){
+        $heightValidation.html('高度不能为空');
+        $height.select();
+      }else{
+        var p = 2 * (w + h),
           a = w * h;
-      var r = new Rectangle(w, h);
-      $perimeter.val(r.perimeter());
-      $area.val(r.area());          
+        var r = new Rectangle(w, h);
+        $perimeter.val(r.perimeter());
+        $area.val(r.area());  
+      }         
     });
 
 });
